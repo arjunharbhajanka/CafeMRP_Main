530a2a35a325b536bf1fc9c677d81cfb29f5d4d1
abh17540 <54676798+abh17540@users.noreply.github.com>
2020-05-24 13:02:40 +0530

    WRITES TO DATBASE!!!!!

--- test.php
+++ test.php
@@ -4,5 +4,44 @@
     $qty = $_POST['qty']; // the key we sent was "something"
     $f = fopen('file.txt', 'a');
     fwrite($f, $name);
-    fclose($f);
+
+
+    $servername = "cafedb.ciecljyjxudo.us-east-2.rds.amazonaws.com";
+    $username = "cafeMRP";
+    $password = "cafeMRP123";
+    $dbname = "cafedb";
+    echo "123";
+    fwrite($f, "\npoint 1\n");
+
+    try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+    echo "123";
+    fwrite($f, "\npoint 2\n");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("INSERT INTO `cafedb`.`orders` (`Item name`, `qty`) VALUES ('$name', '$qty')");
+    $stmt->execute();
+    }
+    catch(PDOException$e) {
+    fwrite($f, $e ->getMessage());
+    echo "Error: ".$e ->getMessage();
+    }
+
+fclose($f);
+
+
+//     // Create connection:w
+//     $conn = new mysqli($servername, $username, $password, $dbname);
+//     // Check connection
+//     if ($conn->connect_error) {
+//       die("Connection failed: " . $conn->connect_error);
+//     }
+//
+//     $sql = "INSERT INTO orders (Item name)
+//     VALUES ('JOHn')";
+//
+//     if ($conn->query($sql) === TRUE) {
+//       echo "New record created successfully";
+//     } else {
+//       echo "Error: " . $sql . "<br>" . $conn->error;
+//     }
 ?>
